f96ea980b5c88f0fcd9c18aebf0f60e351b8d655
chhsueh <ihcaod@gmail.com>
2014-04-03 16:41:57 +0800

    改class的介面 丞宏

--- 提議連署/介面.py
+++ 提議連署/介面.py
@@ -18,42 +18,45 @@ def 全部提議資料(request):
 	文 = RequestContext(request, {
 		'全部資料': 全部資料,
 	})
-	掠pdf()
 	return HttpResponse(版.render(文))
 
-def 提議網頁(request):
-	版 = loader.get_template('提議連署/提議.html')
-	文 = RequestContext(request, {
-	})
-	return HttpResponse(版.render(文))
+class 提議網頁(View):
+	def get(self, request, *args, **kwargs):
+		版 = loader.get_template('提議連署/提議.html')
+		文 = RequestContext(request, {
+		})
+		return HttpResponse(版.render(文))
+	def post(self, request, *args, **kwargs):
+		self.掠pdf()
+		return HttpResponse(request.POST)
 
-def 掠pdf():
+	def 掠pdf(self):
+		'''
+		連署書 PDF 範本初稿已完成：
+		提議：http://www.uisltsc.com.tw/appendectomy/proposal.php
+		連署：http://www.uisltsc.com.tw/appendectomy/petition.php
+		使用方式：可直接使用 <FORM> 的 ACTION 來呼叫，METHOD=POST
+		細節說明：
+		FORM 中 Input Item 的命名規則如下：（序號皆自 0 開始）
+		姓名：Name_[序號]：Name_0,Name_1...
+		身分證字號：IDNo_[序號]
+		性別：Sex_[序號]
+		生日：Birthday_[序號]
+		職業：Occupation_[序號]
+		戶籍地：RegAdd_[序號]
+		QRCode 影像：QRImgPath_[序號]
 	'''
-	連署書 PDF 範本初稿已完成：
-	提議：http://www.uisltsc.com.tw/appendectomy/proposal.php
-	連署：http://www.uisltsc.com.tw/appendectomy/petition.php
-	使用方式：可直接使用 <FORM> 的 ACTION 來呼叫，METHOD=POST
-	細節說明：
-	FORM 中 Input Item 的命名規則如下：（序號皆自 0 開始）
-	姓名：Name_[序號]：Name_0,Name_1...
-	身分證字號：IDNo_[序號]
-	性別：Sex_[序號]
-	生日：Birthday_[序號]
-	職業：Occupation_[序號]
-	戶籍地：RegAdd_[序號]
-	QRCode 影像：QRImgPath_[序號]
-'''
-	參數 = {'Name_0' : 'www', 'IDNo_0' : '123456',
-		'Sex_0' : 'www', 'Birthday_0' : '123456',
-		'Occupation_0' : 'www', 'RegAdd_0' : '123456',
-		'QRImgPath_0' : 'www', }
-	print(urllib.parse.urlencode(參數))
-	連線 = urllib.request.urlopen(
-		url = 'http://www.uisltsc.com.tw/appendectomy/proposal.php',
-		data = urllib.parse.urlencode(參數).encode(encoding='utf_8')
-	)
-	資料=連線.read()
-	連線.close()
-	檔案=open('aa.pdf','wb')
-	檔案.write(資料)
-	檔案.close()
+		參數 = {'Name_0' : 'www', 'IDNo_0' : '123456',
+			'Sex_0' : 'www', 'Birthday_0' : '123456',
+			'Occupation_0' : 'www', 'RegAdd_0' : '123456',
+			'QRImgPath_0' : 'www', }
+		print(urllib.parse.urlencode(參數))
+		連線 = urllib.request.urlopen(
+			url = 'http://www.uisltsc.com.tw/appendectomy/proposal.php',
+			data = urllib.parse.urlencode(參數).encode(encoding='utf_8')
+		)
+		資料=連線.read()
+		連線.close()
+		檔案=open('aa.pdf','wb')
+		檔案.write(資料)
+		檔案.close()
