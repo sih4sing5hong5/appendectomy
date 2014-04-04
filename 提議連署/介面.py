e54cd16f337dd8ce6edf8872a69e7aeacc73bb52
Ihc <Ihc@gmail.com>
2014-04-04 11:53:10 +0800

    pdf會當下載或是傳去7-11分流　丞宏

--- 提議連署/介面.py
+++ 提議連署/介面.py
@@ -21,14 +21,26 @@ def 全部提議資料(request):
 	return HttpResponse(版.render(文))
 
 class 提議網頁(View):
+	版 = loader.get_template('提議連署/提議.html')
+	網址 = 'http://www.uisltsc.com.tw/appendectomy/proposal.php'
+	下載pdf = '下載pdf'
+	上傳至7_11列印 = '上傳至7-11列印'
 	def get(self, request, *args, **kwargs):
-		版 = loader.get_template('提議連署/提議.html')
 		文 = RequestContext(request, {
+			'方式':[self.下載pdf, self.上傳至7_11列印]
 		})
-		return HttpResponse(版.render(文))
+		return HttpResponse(self.版.render(文))
 	def post(self, request, *args, **kwargs):
-		self.掠pdf()
-		return HttpResponse(request.POST)
+		pdf資料 = self.掠pdf()
+		if request.POST['送出'] == self.下載pdf:
+			回應 = HttpResponse()
+			回應.write(pdf資料)
+			回應['Content-Type'] = 'application/pdf'
+			回應['Content-Length'] = len(pdf資料)
+			return 回應
+		elif request.POST['送出'] == self.上傳至7_11列印:
+			return HttpResponse(request.POST['送出'])
+		return self.get(request, *args, **kwargs)
 
 	def 掠pdf(self):
 		'''
@@ -45,18 +57,20 @@ class 提議網頁(View):
 		職業：Occupation_[序號]
 		戶籍地：RegAdd_[序號]
 		QRCode 影像：QRImgPath_[序號]
-	'''
+		'''
 		參數 = {'Name_0' : 'www', 'IDNo_0' : '123456',
 			'Sex_0' : 'www', 'Birthday_0' : '123456',
 			'Occupation_0' : 'www', 'RegAdd_0' : '123456',
 			'QRImgPath_0' : 'www', }
-		print(urllib.parse.urlencode(參數))
+# 		print(urllib.parse.urlencode(參數))
 		連線 = urllib.request.urlopen(
-			url = 'http://www.uisltsc.com.tw/appendectomy/proposal.php',
-			data = urllib.parse.urlencode(參數).encode(encoding='utf_8')
+			url = self.網址,
+			data = urllib.parse.urlencode(參數).encode(encoding = 'utf_8')
 		)
-		資料=連線.read()
+		資料 = 連線.read()
 		連線.close()
-		檔案=open('aa.pdf','wb')
-		檔案.write(資料)
-		檔案.close()
+		return 資料
+
+class 連署網頁(提議網頁):
+	版 = loader.get_template('提議連署/連署.html')
+	網址 = 'http://www.uisltsc.com.tw/appendectomy/petition.php'
