edfce393cf9b11e8366f95cd0bba90778b88c3ab
leo811006 <leo811006@yahoo.com.tw>
2014-04-07 15:42:07 +0800

    qr

--- 提議連署/介面.py
+++ 提議連署/介面.py
@@ -91,6 +91,6 @@ def QRCode(request, 資料):
 	回應 = HttpResponse()
 # 	回應.write(目標.getbuffer())	
 	png圖片.save(回應 , "JPEG")
-	回應['Content-Type'] = 'image/JPEG'
+	回應['Content-Type'] = 'image/PNG'
 # 	回應['Content-Length'] = len(目標.getbuffer())
 	return 回應
